e3afef9c43564dc6ea408528065e940d58db5e73
pavan <pavan@192.168.1.34>
2024-01-10 15:23:49 +0530

    [pavan]:example1

---
+++ src/main/java/practice2/ex1.java
@@ -0,0 +1,12 @@
+package practice2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ex1 {
+
+	public static void main(String[] args) 
+	{
+		WebDriver driver=new ChromeDriver();
+	}
+}
